9e43f258bafb6e64fd67348f46e4fdd49e5d97ae
jsyx1994 <526593251@qq.com>
2017-04-26 09:53:19 +0800

    media root fixed,activities/views.py logic fixed

--- origin2_0/settings.py
+++ origin2_0/settings.py
@@ -126,7 +126,7 @@ USE_TZ = False
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,"static/")
 MEDIA_URL = '/media/'
-#MEDIA_ROOT = os.path.join(BASE_DIR,"media/")
+MEDIA_ROOT = os.path.join(BASE_DIR,"media/")
 
 STATICFILES_DIRS = (
             os.path.join(BASE_DIR, "common_static"),
