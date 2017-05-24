045b6d37d0dac4635fcfac708ae4e98f8275604e
jsyx1994 <526593251@qq.com>
2017-05-24 22:58:59 +0800

    diary backend finished

--- Diaries/views.py
+++ Diaries/views.py
@@ -19,6 +19,13 @@ def edit(request,act_id):
 			content = form.cleaned_data['content']
 			title = form.cleaned_data['title']
 			finished_time = timezone.now()
+			diary = Diary(
+				author = auth,
+				title = title,
+				finished_time = finished_time,
+				content = content,
+				)
+			diary.save()
 			return HttpResponse(finished_time)
 	else:
 		form = PostForm()
